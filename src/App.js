b08f859180c0d005eb7c63ddbe3eecf661c4d864
Daniel Wilstrop <daniel.wilstrop@outlook.com>
2021-07-04 18:58:51 +0100

    minor fixes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import NavBar from './components/NavBar'
 import PokeDex from './pages/PokeDex'
 import Error from './pages/Error'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <NavBar />
       <Switch>
         <Route path='/'>
           <PokeDex />
